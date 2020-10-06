e8bc9e010ac6491d85962b75b3e5bb2f4713efac
nam20 <43489607+nam20@users.noreply.github.com>
2020-10-06 19:43:22 +0900

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,90 +2,69 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
+        <v-card
+        width="400px"
+        class="mx-auto my-5 pa-5">
+          <v-text-field 
+          placeholder="검색"
+          v-model="search"/>
+        </v-card>
+        <v-card
+        width="800px"
+        class="mx-auto my-5 pa-5">
+            <v-btn
+            class="ma-2"
+            :href="`http://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=default_mysearch&searchword=${search}`"
             target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
+            >사람인</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`http://www.jobkorea.co.kr/recruit/joblist?menucode=search`"
             target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
+            >잡코리아</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://work.mma.go.kr/caisBYIS/search/byjjecgeomsaek.do?eopjong_gbcd=1&eopche_nm=${search}`"
             target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
+            >병역일터</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://www.rocketpunch.com/jobs?hiring_types=3`"
             target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+            >로켓펀치</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://www.wanted.co.kr/search?query=${search}`"
+            target="_blank"
+            >원티드</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://job.incruit.com/jobdb_list/searchjob.asp?kw=%BA%B4%BF%AA%C6%AF%B7%CA&rgn2=18&rgn2=11`"
+            target="_blank"
+            >인크루트</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://www.jobplanet.co.kr/job/search?_rs_act=index&_rs_con=job&_rs_element=main_search_bar&q=${search}`"
+            target="_blank"
+            >잡플래닛</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://kreditjob.com/`"
+            target="_blank"
+            >크레딧잡</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`https://www.work.go.kr/empInfo/empInfoSrch/list/dtlEmpSrchList.do?careerTo=&keywordJobCd=&occupation=&rot2WorkYn=&templateInfo=&payGbn=&resultCnt=10&keywordJobCont=&cert=&cloDateStdt=&moreCon=&minPay=&codeDepth2Info=11000&isChkLocCall=&sortFieldInfo=DATE&major=&resrDutyExcYn=&sortField=DATE&staArea=&sortOrderBy=DESC&keyword=&termSearchGbn=all&benefitSrchAndOr=O&disableEmpHopeGbn=&webIsOut=&actServExcYn=Y&keywordStaAreaNm=&maxPay=&emailApplyYn=&listCookieInfo=DTL&pageCode=&codeDepth1Info=11000&keywordEtcYn=&publDutyExcYn=&keywordJobCdSeqNo=&exJobsCd=&templateDepthNmInfo=&computerPreferential=&regDateStdt=&employGbn=&empTpGbcd=1%2C2&region=11000%2C41000&resultCntInfo=10&siteClcd=all&cloDateEndt=&sortOrderByInfo=DESC&currntPageNo=1&indArea=&careerTypes=&searchOn=Y&subEmpHopeYn=&academicGbn=&foriegn=&templateDepthNoInfo=&mealOfferClcd=&station=&moerButtonYn=Y&holidayGbn=&enterPriseGbn=all&academicGbnoEdu=noEdu&cloTermSearchGbn=all&keywordWantedTitle=&stationNm=&benefitGbn=&keywordFlag=&essCertChk=&isEmptyHeader=&depth2SelCode=&_csrf=d5596721-f1b3-49b0-aefa-28962c8a1bee&keywordBusiNm=&preferentialGbn=all&rot3WorkYn=&pfMatterPreferential=&regDateEndt=&staAreaLineInfo1=11000&staAreaLineInfo2=1&pageIndex=1&termContractMmcnt=&careerFrom=&laborHrShortYn=#viewSPL`"
+            target="_blank"
+            >워크넷</v-btn>
+            <v-btn
+            class="ma-2"
+            :href="`http://search.career.co.kr/?kw=%uBCD1%uC5ED%uD2B9%uB840&page=1&sort=1&ac=&jc=&ct=&cr=&biz=&pay=`"
+            target="_blank"
+            >커리어</v-btn>
+        </v-card>
+        
+        
       </v-col>
     </v-row>
   </v-container>
@@ -95,57 +74,17 @@
   export default {
     name: 'HelloWorld',
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+    data(){
+      return {
+        search: ''
+      }
+    },
+   
+   
+    
   }
 </script>
+
+<style scoped>
+
+</style>
