73a8a29796ee9123536bfb34b3be2a185c2f18ed
nam20 <43489607+nam20@users.noreply.github.com>
2020-12-04 11:42:12 +0900

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,12 +14,12 @@
         class="mx-auto my-5 pa-5">
             <v-btn
             class="ma-2"
-            :href="`http://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=default_mysearch&searchword=${search}`"
+            :href="saraminUrl"
             target="_blank"
             >사람인</v-btn>
             <v-btn
             class="ma-2"
-            :href="`http://www.jobkorea.co.kr/Search/?stext=${search}`"
+            :href="jobKoreaUrl"
             target="_blank"
             >잡코리아</v-btn>
             <v-btn
@@ -29,17 +29,17 @@
             >병역일터</v-btn>
             <v-btn
             class="ma-2"
-            :href="`https://www.rocketpunch.com/jobs?hiring_types=3`"
+            :href="rocketUrl"
             target="_blank"
             >로켓펀치</v-btn>
             <v-btn
             class="ma-2"
-            :href="`https://www.wanted.co.kr/search?query=%EC%82%B0%EC%97%85%EA%B8%B0%EB%8A%A5%EC%9A%94%EC%9B%90`"
+            :href="wantedUrl"
             target="_blank"
             >원티드</v-btn>
             <v-btn
             class="ma-2"
-            :href="`https://job.incruit.com/jobdb_list/searchjob.asp?kw=%BA%B4%BF%AA%C6%AF%B7%CA&rgn2=18&rgn2=11`"
+            :href="incruitUrl"
             target="_blank"
             >인크루트</v-btn>
             <v-btn
@@ -54,7 +54,7 @@
             >크레딧잡</v-btn>
             <v-btn
             class="ma-2"
-            :href="`https://www.work.go.kr/empInfo/empInfoSrch/list/dtlEmpSrchList.do?careerTo=&keywordJobCd=&occupation=&rot2WorkYn=&templateInfo=&payGbn=&resultCnt=10&keywordJobCont=&cert=&cloDateStdt=&moreCon=&minPay=&codeDepth2Info=11000&isChkLocCall=&sortFieldInfo=DATE&major=&resrDutyExcYn=&sortField=DATE&staArea=&sortOrderBy=DESC&keyword=&termSearchGbn=all&benefitSrchAndOr=O&disableEmpHopeGbn=&webIsOut=&actServExcYn=Y&keywordStaAreaNm=&maxPay=&emailApplyYn=&listCookieInfo=DTL&pageCode=&codeDepth1Info=11000&keywordEtcYn=&publDutyExcYn=&keywordJobCdSeqNo=&exJobsCd=&templateDepthNmInfo=&computerPreferential=&regDateStdt=&employGbn=&empTpGbcd=1%2C2&region=11000%2C41000&resultCntInfo=10&siteClcd=all&cloDateEndt=&sortOrderByInfo=DESC&currntPageNo=1&indArea=&careerTypes=&searchOn=Y&subEmpHopeYn=&academicGbn=&foriegn=&templateDepthNoInfo=&mealOfferClcd=&station=&moerButtonYn=Y&holidayGbn=&enterPriseGbn=all&academicGbnoEdu=noEdu&cloTermSearchGbn=all&keywordWantedTitle=&stationNm=&benefitGbn=&keywordFlag=&essCertChk=&isEmptyHeader=&depth2SelCode=&_csrf=d5596721-f1b3-49b0-aefa-28962c8a1bee&keywordBusiNm=&preferentialGbn=all&rot3WorkYn=&pfMatterPreferential=&regDateEndt=&staAreaLineInfo1=11000&staAreaLineInfo2=1&pageIndex=1&termContractMmcnt=&careerFrom=&laborHrShortYn=#viewSPL`"
+            :href="worknetUrl"
             target="_blank"
             >워크넷</v-btn>
             <v-btn
@@ -67,6 +67,11 @@
             :href="`https://map.naver.com/v5/search/${search}?c=14141208.4375751,4517939.0043474,15,0,0,0,dh`"
             target="_blank"
             >네이버 지도</v-btn>
+            <v-btn
+            class="ml-10"
+            @click="searchClear">
+              지우기
+            </v-btn>
             
         </v-card>
         
@@ -85,6 +90,35 @@
         search: ''
       }
     },
+    computed:{
+      jobKoreaUrl(){
+        return this.search !== '' ? `http://www.jobkorea.co.kr/Search/?stext=${this.search}` : `http://www.jobkorea.co.kr/recruit/joblist?menucode=search&local=I000,B000&jobtype=9`
+      },
+      saraminUrl(){
+        return this.search !== '' ? `http://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=default_mysearch&searchword=${this.search}`
+          : 'http://www.saramin.co.kr/zf_user/search?loc_mcd=101000%2C102000&job_type=3&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&exc_keyword=%EC%A0%84%EB%AC%B8%EC%97%B0%EA%B5%AC%EC%9A%94%EC%9B%90&panel_type=&search_optional_item=y&search_done=y&panel_count=y'
+      },
+      rocketUrl(){
+        return this.search !== '' ? `https://www.rocketpunch.com/search?keywords=${this.search}` : 'https://www.rocketpunch.com/jobs?hiring_types=3'
+      },
+      wantedUrl(){
+        return this.search !== '' ? `https://www.wanted.co.kr/search?query=${this.search}` : 'https://www.wanted.co.kr/search?query=%EC%82%B0%EC%97%85%EA%B8%B0%EB%8A%A5%EC%9A%94%EC%9B%90'
+      },
+      incruitUrl(){
+        return this.search !== '' ? `https://search.incruit.com/list/search.asp?col=job&src=gsw*search&kw=${this.search}` : 'https://job.incruit.com/jobdb_list/searchjob.asp?occ1=150&kw=%BA%B4%BF%AA%C6%AF%B7%CA&rgn2=11&rgn2=18'
+      },
+      worknetUrl(){
+        return this.search !== '' ? `https://www.work.go.kr/wnSearch/unifSrch.do?regDateStdt=&regDateEndt=&colName=tb_workinfo&srchDateSelected=all&sortField=RANK&sortOrderBy=DESC&searchDateInfo=&temp=&pageIndex=1&tabName=tb_workinfo&dtlSearch=&query=${this.search}&radio_period=on&srchStdt=&srchEndt=&reQuery=&agreeQuery=&prikeyQuery=&exceptQuery=`
+          : 'https://www.work.go.kr/empInfo/empInfoSrch/list/dtlEmpSrchList.do?careerTo=&keywordJobCd=&occupation=&rot2WorkYn=&templateInfo=&payGbn=&resultCnt=10&keywordJobCont=&cert=&cloDateStdt=&moreCon=&minPay=&codeDepth2Info=11000&isChkLocCall=&sortFieldInfo=DATE&major=&resrDutyExcYn=&sortField=DATE&staArea=&sortOrderBy=DESC&keyword=&termSearchGbn=all&benefitSrchAndOr=O&disableEmpHopeGbn=&webIsOut=&actServExcYn=Y&keywordStaAreaNm=&maxPay=&emailApplyYn=&listCookieInfo=DTL&pageCode=&codeDepth1Info=11000&keywordEtcYn=&publDutyExcYn=&keywordJobCdSeqNo=&exJobsCd=&templateDepthNmInfo=&computerPreferential=&regDateStdt=&employGbn=&empTpGbcd=1%2C2&region=11000%2C41000&resultCntInfo=10&siteClcd=all&cloDateEndt=&sortOrderByInfo=DESC&currntPageNo=1&indArea=&careerTypes=&searchOn=Y&subEmpHopeYn=&academicGbn=&foriegn=&templateDepthNoInfo=&mealOfferClcd=&station=&moerButtonYn=Y&holidayGbn=&enterPriseGbn=all&academicGbnoEdu=noEdu&cloTermSearchGbn=all&keywordWantedTitle=&stationNm=&benefitGbn=&keywordFlag=&essCertChk=&isEmptyHeader=&depth2SelCode=&_csrf=d5596721-f1b3-49b0-aefa-28962c8a1bee&keywordBusiNm=&preferentialGbn=all&rot3WorkYn=&pfMatterPreferential=&regDateEndt=&staAreaLineInfo1=11000&staAreaLineInfo2=1&pageIndex=1&termContractMmcnt=&careerFrom=&laborHrShortYn=#viewSPL'
+      },
+      
+
+    },
+    methods:{
+      searchClear(){
+        this.search = ''
+      }
+    }
    
    
     
